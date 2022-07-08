4e8d3f3d578ef1d76ab62c3022f3c4a1e32eed5c
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2022-07-08 06:19:45 -0700

    peectures

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Credit: Adam Smith
 Modified by: Keenan Rea
 
-Description:
+Description: A image generator, fed pixel art land scape shots
 
-Artist Statement:
+Artist Statement: I like pixel art, so my goal was to get the generator to try to imiate the style of the three pixel art landscape shots I gave it. Of course, I knew it was probably not gonna hold up to the standards of the art I gave it
 
--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 
 function p4_inspirations() {
-  return [];
+  return [ (mountainsea, "https://preview.redd.it/rc2xyhpx49m71.gif?format=png8&s=c6df050def34d23bd7ffb860ac7cb6c017cea33e"), ];
 }
+// https://preview.redd.it/rc2xyhpx49m71.gif?format=png8&s=c6df050def34d23bd7ffb860ac7cb6c017cea33e
+// https://cdn.mos.cms.futurecdn.net/EFXSes9UCfsyRVoNeQ2ZTB-1200-80.png
+// https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/116770670/original/281f85ad141b056fc82fa8058134a86421c61817/paint-you-a-beautiful-pixel-art-landscape.png
 
 function p4_initialize(inspiration) {
   return {};
